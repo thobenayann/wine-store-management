bccd30161d1a4b5e5896d4e8573f14e0eeaaf39f
thobenayann <thobena.yann@orange.fr>
2024-05-25 21:54:20 +0200

    first migration and create user

--- actions/register.ts
+++ actions/register.ts
@@ -1,16 +1,44 @@
 'use server';
 
+import prisma from '@/lib/db';
 import { RegisterSchema } from '@/schemas';
+import bcrypt from 'bcrypt';
 import { z } from 'zod';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values);
 
-    if (!validatedFields) {
+    if (!validatedFields.success) {
         return {
             error: 'Invalid fields',
         };
     }
 
-    return { success: 'Email sent!' };
+    const { email, password, firstName, lastName } = values;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Check if user already exists
+    const existingUser = await prisma.user.findUnique({
+        where: {
+            email,
+        },
+    });
+
+    if (existingUser)
+        return { error: 'Cette adresse email est déjà utilisée.' };
+
+    // Create user
+    await prisma.user.create({
+        data: {
+            email,
+            password: hashedPassword,
+            first_name: firstName,
+            last_name: lastName,
+            role: 'user',
+        },
+    });
+
+    // TODO: Send verification token email
+
+    return { success: 'Utilisateur créé !' };
 };
--- setup.md
+++ setup.md
@@ -4,4 +4,10 @@
 
 -   NextJS : `npx create-next-app@latest project-name`
 -   Shadcn UI : `npx shadcn-ui@latest init`
--   ``
+-   Prisma : `npm i -D prisma`
+-   Prisma Client : `npm install @prisma/client`
+-   Prisma Init : `npx prisma init`
+-   NextAuth Prisma adapter : `npm i @auth/prisma-adapter`
+-   Prisma first migration : `prisma migrate dev --name init`
+-   Bcrypt : `npm i bcrypt`
+-   Bcrypt types : `npm i -D @types/bcrypt`
