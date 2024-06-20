c531ebd0b988b570c425625eaad61df0cd217b3f
thobenayann <thobena.yann@orange.fr>
2024-06-20 16:39:42 +0200

    update prisma schema and create user settings

--- actions/register.ts
+++ actions/register.ts
@@ -19,32 +19,49 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const { email, password, firstName, lastName } = values;
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Check if user already exists
-    const existingUser = await prisma.user.findUnique({
-        where: {
-            email,
-        },
-    });
+    try {
+        // Check if user already exists
+        const existingUser = await prisma.user.findUnique({
+            where: { email },
+        });
 
-    if (existingUser)
-        return { error: 'Cette adresse email est déjà utilisée.' };
+        if (existingUser) {
+            return { error: 'Cette adresse email est déjà utilisée.' };
+        }
 
-    // Create user
-    await prisma.user.create({
-        data: {
-            email,
-            password: hashedPassword,
-            first_name: firstName,
-            last_name: lastName,
-        },
-    });
+        // Start a transaction
+        const user = await prisma.$transaction(async (prisma) => {
+            // Create user with default userSettings
+            const newUser = await prisma.user.create({
+                data: {
+                    email,
+                    password: hashedPassword,
+                    first_name: firstName,
+                    last_name: lastName,
+                },
+            });
 
-    // Send verification token email
-    const verificationToken = await generateVerificationToken(email);
-    await sendVerificationEmail(
-        verificationToken.email,
-        verificationToken.token
-    );
+            // Create user settings
+            await prisma.userSettings.create({
+                data: {
+                    user_id: newUser.id,
+                    // No need to specify vat_rate, payment_terms, or currency since they have defaults
+                },
+            });
 
-    return { success: 'Email de confirmation envoyé.' };
+            // Send verification token email
+            const verificationToken = await generateVerificationToken(email);
+            await sendVerificationEmail(
+                verificationToken.email,
+                verificationToken.token
+            );
+
+            return newUser;
+        });
+
+        return { success: 'Email de confirmation envoyé.' };
+    } catch (error) {
+        console.error('Unexpected error:', error);
+        return { error: 'An unexpected error occurred.' };
+    }
 };
